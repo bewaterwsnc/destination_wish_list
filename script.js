ecebcdd5062c6ef8aa0dd6ffbd6e68dc3be2c3d6
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2022-03-01 03:08:52 -0500

    updated reference notes

--- script.js
+++ script.js
@@ -9,37 +9,40 @@
     //create eventListener for html lines 26-44 
     document.querySelector("#details_form")
             .addEventListener("submit", submitHandler);
-
-    //function 2: submit form data
-    function submitHandler(e) {
-
-        //clears form -- (f1)
-        clearForm(e.target);
         
-        //creating destination variables from each form line
-        const destName = e.target.elements["destination_name"].value;
-        const destLocation = e.target.elements["location_name"].value;
-        const destPhoto = e.target.elements["photo"].value;
-        const destDescription = e.target.elements["description"].value
+        /*function 2: submit form data via eventListener (submitHandler)
+          clear form / create event variables / create card / change title of card display area
+          if new list populates / add card to staged area */
+        function submitHandler(e) {
+
+            //clears form -- (f1)
+            clearForm(e.target);
+        
+            //creating destination variables from each form line
+                           //event.target-element-that-triggered-event's.value
+            const destName = e.target.elements["destination_name"].value;
+            const destLocation = e.target.elements["location_name"].value;
+            const destPhoto = e.target.elements["photo"].value;
+            const destDescription = e.target.elements["description"].value
     
-        //store destination variables in a card
-        var displayCard = createDestCard(
-            destName,
-            destLocation,
-            destPhoto,
-            destDescription
-        );
+            //store destination variables in a card
+            var displayCard = createDestCard(
+                destName,
+                destLocation,
+                destPhoto,
+                destDescription
+            );
         
-        //place card_stage in variable to allow title change for html line 47-48
-        var destCardContainer = document.querySelector("#card_stage");
+            //place card_stage in variable to allow title change for html line 47-48
+            var destCardContainer = document.querySelector("#card_stage");
         
-        /*title change for "staging" - "ENTER DESTINATION DETAILS"
-         if the container + is null, set new title to "My Wishlist" */
-        if (destCardContainer.children.length === 0) {
-            document.querySelector("#staging").innerHTML = "My Wishlist";
-        } 
+            /*title change for "staging" - "ENTER DESTINATION DETAILS"
+            if the container + is null, set new title to "My Wishlist" */
+            if (destCardContainer.children.length === 0) {
+                document.querySelector("#staging").innerHTML = "My Wishlist";
+            } 
 
-        //adds form->card to container in html line 51-
-        document.querySelector("#card_stage")
-                .appendChild(displayCard)
-    }
+            //adds form->card to container in html line 51-
+            document.querySelector("#card_stage")
+                    .appendChild(displayCard)
+        }
